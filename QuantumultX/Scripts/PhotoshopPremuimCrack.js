bb06ef778679f472bf1983472f6b54f7f4aa3e89
Piggestpog <81506494+Piggestpog@users.noreply.github.com>
2022-04-10 17:55:04 +0800

    Update PhotoshopPremuimCrack.js

--- QuantumultX/Scripts/PhotoshopPremuimCrack.js
+++ QuantumultX/Scripts/PhotoshopPremuimCrack.js
@@ -1,24 +1,15 @@
 /*
-Quantumult X 脚本
-Unlock Photoshop for iPad & Ps Express
-
-By LTribe
-Photoshop download link : http://t.cn/AiBIDoTv
-Ps Express download link : http://t.cn/AiEwcMje
-
-注意 Adobe PhotoShop 需注册登录非大陆的Adobe账号，否则会没有云存储空间
+Unlock Photoshop for iPad & Ps Express (By LTribe)
+Download Link: http://t.cn/AiBIDoTv (Photoshop for IPad)
+               http://t.cn/AiEwcMje (Photoshop Express)
+Note: Adobe PhotoShop 需注册登录非大陆的Adobe账号，否则会没有云存储空间
 
 [rewrite_local]
-# Photoshop for iPad & Ps Express 解锁内购订阅【By LTribe】
-^https:\/\/.*\.adobe\..*\/mobile_profile\/nul\/v\d$ url script-response-body photoshop.js
-
+^https:\/\/.*\.adobe\..*\/mobile_profile\/nul\/v\d$ url script-response-body https://raw.githubusercontent.com/Piggestpog/Proxies/main/QuantumultX/Scripts/PhotoshopPremuimCrack.js
 [mitm]
 hostname = *.adobe.*
-
 */
 
 let obj = JSON.parse($response.body);
-
 obj.mobileProfile.profileStatus = 'PROFILE_AVAILABLE';
-
 $done({body: JSON.stringify(obj)});
