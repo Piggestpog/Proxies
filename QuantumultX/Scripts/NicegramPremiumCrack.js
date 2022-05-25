2f27e46cdf2becc1cebaaa21ceb8cd9959c88f3c
Piggestpog <81506494+Piggestpog@users.noreply.github.com>
2022-05-25 08:14:37 +0800

    Update NicegramPremiumCrack.js

--- QuantumultX/Scripts/NicegramPremiumCrack.js
+++ QuantumultX/Scripts/NicegramPremiumCrack.js
@@ -1,10 +1,9 @@
 /*
 Nicegram Premium Suscription Unlock (By Cuttlefish)
-Source: https://github.com/ddgksf2013/Cuttlefish/blob/master/Crack/nicegram.js
 Update Date: 2022/4/10
 
 [rewrite_local]
-https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Piggestpog/Proxies/main/QuantumultX/Scripts/NicegramPremiumCrack.js
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/nicegram_body.js
 [mitm]
 hostname=restore-access.indream.app
 */
