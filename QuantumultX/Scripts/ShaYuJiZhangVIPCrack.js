d773d81c2e01db401a3900823add4e13561b8912
Piggestpog <81506494+Piggestpog@users.noreply.github.com>
2022-05-25 08:07:33 +0800

    Create ShaYuJiZhangVIPCrack.js

---
+++ QuantumultX/Scripts/ShaYuJiZhangVIPCrack.js
@@ -0,0 +1,6 @@
+/*
+鲨鱼记账 VIP Subscription Unlock (By Gx3dong)
+Source: https://github.com/Gx3dong/Surge/blob/master/JS/shyjzh.js
+
+[rewrite_local]
+https:\/\/api\.shayujizhang\.com\/account\/grant\/detail\/info url script-response-body 
